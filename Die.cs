1eb8e42c15c5bab2c5a3aa29038066332a3866f4
LeoNaylor04 <leonaylor04@gmail.com>
2024-04-15 11:24:13 +0100

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,16 @@
+﻿namespace DiceGame
+{     internal class Die
+    {
+        private int _Value = 0;
+        public int value 
+        { 
+            get { return _Value; } 
+            set {  _Value = value; } 
+        }
+        public int Roll()
+        {
+            Random random = new Random();
+            return random.Next(1,7);
+        }
+    }
+}
---
+++ Testing.cs
@@ -0,0 +1,7 @@
+﻿internal class Testing
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
